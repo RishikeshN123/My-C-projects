2fe2a102c19b7271375e48231612e2e146b06bc2
RishikeshN123 <rishikeshnofficial@gmail.com>
2025-07-01 20:13:03 +0530

    Update Desimal_to_Binary.c

--- Desimal_to_Binary.c
+++ Desimal_to_Binary.c
@@ -2,12 +2,14 @@
 int main()
 {
   int n;
+//Accept user input
   printf("Enter the number:");
   scanf("%d",&n);
   int q,r,i;
   int a[1024];
   q=n;
   i=0;
+//Converting to binary
   while(q>0)
   {
    r=q%2;
@@ -16,6 +18,8 @@ int main()
    i++;
   }
    printf("Binary Number:");
+//Reversing array and giving output
+//in binary format
    for(int j=i-1;j>=0;j--)
      {
       printf("%d",a[j]);
