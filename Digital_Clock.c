64c466096aec5b05290ba4a94877d75d8b6b7b85
RishikeshN123 <rishikeshnofficial@gmail.com>
2025-07-02 08:38:07 +0530

    Create Digital_Clock.c
    A simple terminal-based digital clock built using C.
    It displays the current system time in HH:MM:SS format and updates every second in real-time using sleep() and system("clear").

---
+++ Digital_Clock.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+//for linex os
+#include <unistd.h>
+//for windows os use Sleep(1000) instead of 
+//sleep(1)
+#include <windows.h>
+#include <time.h>
+//for boolien variables
+#include <stdbool.h>
+#include <stdio.h>
+int main()
+{
+  bool Clock=true;
+  time_t rawtime=0;
+  struct tm *pT=NULL;
+  while(Clock)
+  {
+    time(&rawtime);
+    pT=localtime(&rawtime);
+    system("clear");
+    printf("\t Digital clock:\n");
+    printf("\t %02d:%02d:%02d \n",pT->tm_hour,pT->tm_min,pT->tm_sec);
+    sleep(1);
+  }
+  return 0;
+}
